4a11f7b803c5a06ffa4eea621eef445f717f5d82
Markus <markus@thral.de>
2019-01-25 22:19:23 +0100

    Update header.php

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -7,7 +7,7 @@
 <meta name="viewport"
 	content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<link rel="icon" type="image/png" href="images/layout/Logo.png">
+<link rel="icon" type="image/png" href="<?= $config["urls"]["html"]?>/images/layout/Logo.png">
 
 <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">-->
 <link rel="stylesheet" type="text/css" href='<?= $config["urls"]["html"]?>/css/custom.css'>
