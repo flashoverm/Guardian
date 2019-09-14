36a10fbbe355bda614b2202217196248d2b19f36
flashoverm <markus@thral.de>
2019-09-14 23:25:39 +0200

    Bugfix

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -20,7 +20,7 @@
 	   </div>
 		<div class="row">
 			<div class="col">
-				<a href="<?= $config["urls"]["base_url"]?>">
+				<a href="<?= $config["urls"]["baseUrl"]?>">
 					<img class="img-fluid d-block"
 						src="<?= $config["urls"]["intranet_home"] ?>/images/layout/shortheader_new-1.png">
 				</a>
