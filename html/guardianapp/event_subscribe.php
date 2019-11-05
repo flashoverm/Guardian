4c63dabc8e52a23e397bbda18d2374f88f937428
flashoverm <markus@thral.de>
2019-11-05 20:21:58 +0100

    Bufgix: Subscribe template not existing

--- html/guardianapp/event_subscribe.php
+++ html/guardianapp/event_subscribe.php
@@ -1,6 +1,6 @@
 <?php
 require_once realpath ( dirname ( __FILE__ ) . "/../../resources/config.php" );
-require_once LIBRARY_PATH . "/template.php";
+require_once TEMPLATES_PATH . "/template.php";
 require_once LIBRARY_PATH . "/db_event.php";
 require_once LIBRARY_PATH . "/db_engines.php";
 require_once LIBRARY_PATH . "/db_user.php";
